58965f726a801892acb98e7c00291a53fd0c7cf1
paeyden <origamercy@gmail.com>
2025-11-24 01:01:34 +0300

    fix: removed the db uri string

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -20,7 +20,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('MongoDB connected successfully:', uri);
+        console.log('MongoDB connected successfully.....');
     } catch (error) {
         console.error('MongoDB connection failed:', error.message);
         process.exit(1);
